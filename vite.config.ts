f9d95537464f59df4a5f8754712b7bbb5d5a0f34
José Fernandes <josefern.dev@gmail.com>
2026-01-07 12:00:55 +0000

    Feat material-ui-insert

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,17 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+  optimizeDeps: {
+    include: ['@emotion/styled']
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components')
     }
   }
 })
