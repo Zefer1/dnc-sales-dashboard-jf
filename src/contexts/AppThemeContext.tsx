87981d77e617d9ebaa9b6e6ca689e71bf8f1fd51
José Fernandes <josefern.dev@gmail.com>
2026-01-09 17:54:57 +0000

    Feat: Switch Theme Logic

---
+++ src/contexts/AppThemeContext.tsx
@@ -0,0 +1,31 @@
+import { ReactNode, useEffect, useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme, lightTheme } from '@/styles'
+import { AppThemeContext } from './AppThemeContextValue'
+
+export { AppThemeContext }
+
+export const AppThemeProvider = ({ children }: { children: ReactNode }) => {
+	const savedTheme = localStorage.getItem('theme')
+	const [appTheme, setAppTheme] = useState(savedTheme ?? 'light')
+
+	const toggleTheme = () => {
+		setAppTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'))
+	}
+
+	useEffect(() => {
+		localStorage.setItem('theme', appTheme)
+	}, [appTheme])
+
+	return (
+		<AppThemeContext.Provider value={{ appTheme, toggleTheme }}>
+			<ThemeProvider 
+            theme={appTheme === 'light' ? lightTheme : darkTheme}
+            >
+				{children}
+			</ThemeProvider>
+		</AppThemeContext.Provider>
+	)
+}
+
+export default AppThemeProvider
